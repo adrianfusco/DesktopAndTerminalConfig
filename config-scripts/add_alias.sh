694f95b8deaf25d07e87c90b709d5ea3b8c91168
Adrian <59370927+adrianfusco@users.noreply.github.com>
2022-03-04 23:32:41 +0100

    Update add_alias.sh
    Add git alias to list files changed after commit with `--name-status` and pretty `graph`.

--- config-scripts/add_alias.sh
+++ config-scripts/add_alias.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 cat <<EOT >> $HOME/.bashrc
+
 export PS1="[\$(date '+%Y-%m-%d %H:%M:%S')] {\[\033[0;32m\]\[\033[0m\033[0;32m\]\u\[\033[0;36m\]@\[\033[0;36m\]\h} (\w\[\033[0;33m\])\$(__git_ps1)$\[\033[0m\033[0;32m\] -> "
 ######################################################## Alias ######################################################### 
 alias l="ls -ltrah"
@@ -8,8 +9,10 @@ alias c="xclip -selection clipboard"
 alias dmesg="dmesg --human --kernel --follow --ctime"
 alias random_password="dd if=/dev/urandom bs=1 count=12 2>/dev/null | base64 -w 0 | rev | cut -b 2- | rev"
 alias testing_docker='echo Mounting: \`pwd\`; docker run -it -v \`pwd\`:/tmp/testing_virtualenv testing'
+alias git_list_commited_changes="paste <(git log -1 --stat --oneline --decorate=no --pretty=\"format:\" --name-status) <(git log -1 --stat --oneline --pretty=\"format:\") | column -s $'\t' -tne"
 
 ##### Functions #####
+
 function read_yaml() {
     python3 -c 'import yaml, sys; print(yaml.safe_load(sys.stdin))' < $1
 }
@@ -17,8 +20,8 @@ function read_yaml() {
 function mem() {
     ps -eo rss,pid,euser,args:100 --sort %mem | grep -v grep | grep -i $@ | awk '{printf $1/1024 "MB"; $1=""; print }'
 }
+
 ########################################################################################################################
 EOT
 
 source $HOME/.bashrc
-
